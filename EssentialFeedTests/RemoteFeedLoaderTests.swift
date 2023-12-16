a5324e560ac60bbbf24180d52e100d6175acf266
User <user@MacBook-Pro-de-User.local>
2023-12-17 00:09:35 +0100

    Move test logic to a sparate type

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -10,16 +10,24 @@ import XCTest
 class RemoteFeedLoader {
     
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
     
 }
 
 class HTTPClient {
     
-    static let shared = HTTPClient()
-    private init() {}
+    static var shared = HTTPClient()
     
+    func get(from url: URL) { }
+
+    
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     var requestedURL: URL?
     
 }
@@ -27,8 +35,10 @@ class HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRquestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
+        
         
         XCTAssertNil(client.requestedURL)
     }
@@ -36,11 +46,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     // load item command
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        
+        
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
-        
         sut.load()
-        
         XCTAssertNotNil(client.requestedURL)
     }
 
