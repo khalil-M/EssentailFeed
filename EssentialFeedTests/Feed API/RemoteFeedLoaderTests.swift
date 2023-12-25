349ffda50f111ef88fc7b2f4eb66411d9e13ecab
User <user@MacBook-Pro-de-User.local>
2023-12-25 15:15:47 +0100

    Remove 'LoadResult' Equatable conformance from productipn since it's a test only requirement

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -153,13 +153,26 @@ final class RemoteFeedLoaderTests: XCTestCase {
         return try! JSONSerialization.data(withJSONObject: json)
     }
     
-    private func expect(_ sut: RemoteFeedLoader, toCompleteWith result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath,
-                        line: UInt = #line) {
-        var capturedResults = [RemoteFeedLoader.Result]()
-        sut.load { capturedResults.append($0) }
+    private func expect(_ sut: RemoteFeedLoader, toCompleteWith expectedresult: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
+        let exp = expectation(description: "wait for load completion")
+//        var capturedResults = [RemoteFeedLoader.Result]()
+        sut.load { receivedResult in
+            switch(receivedResult ,expectedresult) {
+            case let (.success(receivedItems), .success(expecteditems)):
+                XCTAssertEqual(receivedItems, receivedItems, file: file, line: line)
+            case let (.failure(receivedError), .failure(expectedError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected result \(expectedresult) got \(receivedResult) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
         
         action()
-        XCTAssertEqual(capturedResults, [result], file: file, line: line)
+        
+        wait(for: [exp], timeout: 1.0)
+        
     }
     
     private class HTTPClientSpy: HTTPClient {
