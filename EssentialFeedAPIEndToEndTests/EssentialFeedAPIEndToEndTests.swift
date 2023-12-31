923633ddf4ebd6358b5ef8ae25f54d4644df4677
User <user@MacBook-Pro-de-User.local>
2023-12-31 14:18:49 +0100

    Add memory leak tracking for components integration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -17,7 +17,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
             XCTAssertEqual(items.count, 8, "Expected 8 items in the test account feed")
             XCTAssertEqual(items[0], expectedItem(at: 0))
             XCTAssertEqual(items[1], expectedItem(at: 1))
-//            XCTAssertEqual(items[1], expectedItem(at: 2))
+            XCTAssertEqual(items[2], expectedItem(at: 2))
             XCTAssertEqual(items[3], expectedItem(at: 3))
             XCTAssertEqual(items[4], expectedItem(at: 4))
             XCTAssertEqual(items[5], expectedItem(at: 5))
@@ -35,11 +35,13 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult() -> LoadFeedResult? {
+    private func getFeedResult(file: StaticString = #filePath,
+                               line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient()
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
-        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         let exp = expectation(description: "Wait for load completion")
         
         var receivedResult: LoadFeedResult?
