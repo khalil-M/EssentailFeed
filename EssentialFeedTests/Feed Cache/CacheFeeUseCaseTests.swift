9af0e878f15f219f1e8df2a3bacb74cb432a55a3
User <user@MacBook-Pro-de-User.local>
2024-01-07 13:54:16 +0100

    Save command requests cache deletion

--- EssentialFeedTests/Feed Cache/CacheFeeUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeeUseCaseTests.swift
@@ -6,15 +6,25 @@
 //
 
 import XCTest
-
+import EssentialFeed
 class LocalFeedLoader {
     
+    let store: FeedStore
+    
     init(store: FeedStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ itesm: [FeedItem]) {
+        store.deledCachedFeed()
     }
 }
+
 class FeedStore {
    var deleteCachedFeedCallcount = 0
+    func deledCachedFeed() {
+        deleteCachedFeedCallcount += 1
+    }
 }
 class CacheFeeUseCaseTests: XCTestCase {
     
@@ -23,5 +33,23 @@ class CacheFeeUseCaseTests: XCTestCase {
         let store = FeedStore()
         _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallcount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        // invoke sut
+        let items = [uniqueItem(), uniqueItem()]
+        sut.save(items)
+        XCTAssertEqual(store.deleteCachedFeedCallcount, 1)
+    }
+    
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+    
+    private func anyURL() -> URL {
+        let url = URL(string: "https://any-url.com")!
+        return url
     }
 }
