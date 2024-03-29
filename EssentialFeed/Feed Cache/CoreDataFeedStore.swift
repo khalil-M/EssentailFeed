a21e86b5c21a7c88a5a2aca8e30946cdbde2b1b3
User <khalil.mhelheli@gmail.com>
2024-03-29 16:33:29 +0100

    Extract model translations into helper methods within the managed models

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -24,12 +24,7 @@ public final class CoreDataFeedStore: FeedStore {
                 let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
                 request.returnsObjectsAsFaults = false
                 if let cache = try context.fetch(request).first {
-                    completion(.found(
-                        feed: cache.feed
-                            .compactMap { ($0 as? ManagedFeedImage) }
-                            .map {
-                                LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
-                            },
+                    completion(.found(feed: cache.localFeed,
                         timestamp: cache.timestamp))
                 } else {
                     completion(.empty)
@@ -46,15 +41,7 @@ public final class CoreDataFeedStore: FeedStore {
             do {
                 let managedCache = ManagedCache(context: context)
                 managedCache.timestamp = timestamp
-                managedCache.feed = NSOrderedSet(array: feed.map { local in
-                    let managed = ManagedFeedImage(context: context)
-                    managed.id = local.id
-                    managed.imageDescription = local.description
-                    managed.location = local.location
-                    managed.url = local.url
-                    return managed
-                })
-                
+                managedCache.feed = ManagedFeedImage.images(from: feed, context: context)
                 try context.save()
                 completion(nil)
             } catch {
@@ -105,9 +92,13 @@ private extension NSManagedObjectModel {
 
 @objc(ManagedCache)
 private class ManagedCache: NSManagedObject {
-     @NSManaged var timestamp: Date
-     @NSManaged var feed: NSOrderedSet
- }
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local}
+    }
+}
 
 @objc(ManagedFeedImage)
  private class ManagedFeedImage: NSManagedObject {
@@ -116,4 +107,20 @@ private class ManagedCache: NSManagedObject {
      @NSManaged var location: String?
      @NSManaged var url: URL
      @NSManaged var cache: ManagedCache
+     
+     static func images(from localFeed: [LocalFeedImage], context: NSManagedObjectContext) -> NSOrderedSet {
+         return NSOrderedSet(array: localFeed.map { local in
+             let managed = ManagedFeedImage(context: context)
+             managed.id = local.id
+             managed.imageDescription = local.description
+             managed.location = local.location
+             managed.url = local.url
+             return managed
+         })
+     
+     }
+     
+     var local: LocalFeedImage {
+         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+     }
  }
