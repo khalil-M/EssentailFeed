003e4df3c7d225b07f339d7da902458933ad3307
User <khalil.mhelheli@gmail.com>
2024-04-01 15:17:12 +0200

    Include cache test helpers in the cache integration tests target

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -20,11 +20,40 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
                 XCTAssertEqual(imageFeed, [], "Expected empty feed")
                 
             case let .failure(error):
-                XCTFail("Expected successfully feed result, got \(error) instead")
+                XCTFail("Expected successful feed result, got \(error) instead")
             }
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_load_deliversItemsSavedOnSeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerforomLoad = makeSUT()
+        
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save successfully")
+            saveExp.fulfill()
+        }
+        
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerforomLoad.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, feed)
+                
+            case let .failure(error):
+                XCTFail("Expected successfull feed resutl, got \(error) instead")
+            }
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1.0)
     }
     
     // MARK: Helpers
