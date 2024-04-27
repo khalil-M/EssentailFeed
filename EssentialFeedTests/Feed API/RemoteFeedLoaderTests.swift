c62fe37c7c10756873e118b7fe03b39196ed3886
User <khalil.mhelheli@gmail.com>
2024-04-27 13:34:49 +0200

    Replace custom 'HTTPClientResult' enum with standard 'Swift.Result

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -180,12 +180,12 @@ final class RemoteFeedLoaderTests: XCTestCase {
     private class HTTPClientSpy: HTTPClient {
        
         
-        private var messages = [(url: URL, completion:(HTTPClientResult) -> Void)]()
+        private var messages = [(url: URL, completion:(HTTPClient.Result) -> Void)]()
         
         var requestedURLs: [URL] {
             return messages.map { $0.url }
         }
-        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
             messages.append((url, completion))
         }
         
@@ -196,7 +196,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         
         func complete(withStatusCode code: Int, data: Data, at index: Int = 0) {
             let response = HTTPURLResponse(url: requestedURLs[index], statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(data, response))
+            messages[index].completion(.success((data, response)))
         }
     }
 
