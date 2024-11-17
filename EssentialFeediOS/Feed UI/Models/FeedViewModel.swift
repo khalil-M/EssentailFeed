77077c4945357b57cb93723419f787cf7d1f5a14
User <khalil.mhelheli@gmail.com>
2024-11-17 16:42:24 +0100

    Move 'FeedLoader' loading state to 'FeedViewModel'. Now the 'FeedRefreshViewController' acts as a binder between the 'View' and the 'ViewModel'

---
+++ EssentialFeediOS/Feed UI/Models/FeedViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by User on 17/11/2024.
+//
+
+import EssentialFeed
+
+final class FeedViewModel {
+    //since the viewModel manage the feedLoading state
+    
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+//    private enum State {
+//        case pending
+//        case loading
+//    }
+    // viewModel can capture the state in private property and every time there is a state change
+    // we notify the observer onChange closure
+//    private var state = State.pending {
+//        didSet { onChange?(self)}
+//    }
+    
+    //normally using combine or RxSwift we can use a simple closure
+    var onChange: ((FeedViewModel) -> Void)?
+    var onFeedLoad: (([FeedImage])->Void)?
+    
+    //but since the state is private we need to expode access for the current state of the viewModel
+    
+    private (set) var isLoading: Bool = false {
+        didSet { onChange?(self)}
+    }
+    
+    
+    func loadFeed() {
+        //define the state transition
+//        state = .loading
+        isLoading = true
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                //                self?.state = .loaded(feed)
+                self?.onFeedLoad?(feed)
+            }
+            self?.isLoading = false
+            
+        }
+    }
+}
